fd3b153ee61c1f165af3e48773f832def6bd44c5
Gahyeon19 <wjsrkgus0119@naver.com>
2024-11-27 15:23:20 +0900

    cicd test using mysql 08

--- src/test/java/com/example/cicd_test_3/CicdTest3ApplicationTests.java
+++ src/test/java/com/example/cicd_test_3/CicdTest3ApplicationTests.java
@@ -3,10 +3,10 @@ package com.example.cicd_test_3;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class CicdTest3ApplicationTests {
 
-    @Test
+//    @Test
     void contextLoads() {
     }
 
